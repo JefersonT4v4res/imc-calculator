e8fa2dfc818a9e153a8496d603b5aeb5a32a2fc5
Jeferson Lucas Tavares <63317938+JefersonT4v4res@users.noreply.github.com>
2023-03-16 11:38:55 -0300

    :update: Making changes to 'getFilecsv' and 'peopleData'

--- src/getFileCSV.java
+++ src/getFileCSV.java
@@ -10,17 +10,34 @@ public class getFileCSV {
         try {
             File csvFile = new File("dao/DATASET.csv");
             Scanner reader = new Scanner(csvFile);
+            List<peopleData> p = new ArrayList<peopleData>(); 
 
             while (reader.hasNextLine()) {
                 String line = reader.nextLine();
                 String[] fileDatas = line.split(",");
-            
-                for (String data : fileDatas) {
-                   System.out.print(data + " ");
+                System.out.println(fileDatas.length);
+                if (fileDatas.length == 4) {
+                String firstName = fileDatas[0];
+                String surname = fileDatas[1];
+                Float weight = Float.parseFloat(fileDatas[2]);
+                Float height = Float.parseFloat(fileDatas[3]);
+                
+                peopleData person = new peopleData(firstName, surname, weight, height);
+                p.add(person);
+                } else {
+                    //System.out.println("Invalid line " + line);
                 }
-                System.out.println();
+                
             }
             reader.close();
+
+            for (peopleData person : p) {
+                System.out.println("Nome: " + ((peopleData) p).getFirstName());
+                System.out.println("Sobrenome: " + ((peopleData) p).getSurname());
+                System.out.println("Peso: " + ((peopleData) p).getWeight());
+                System.out.println("Altura: " + ((peopleData) p).getHeight());
+                System.out.println();
+             }
 
          } catch (FileNotFoundException e) {
             System.out.println("Erro ao abrir o arquivo.");
