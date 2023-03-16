00a56440c4129d442e9742f0d2b19aff0a14b089
Jeferson Lucas Tavares <63317938+JefersonT4v4res@users.noreply.github.com>
2023-03-16 13:27:10 -0300

    :update: Reading and Writting the informations of the '.csv'

--- src/getFileCSV.java
+++ src/getFileCSV.java
@@ -5,39 +5,43 @@ public class getFileCSV {
     
     public static void main(String[] args) {
 
-        //Reader reader = Files.newBufferedReader(Paths.get(""));
-
-        try {
+         try {
             File csvFile = new File("dao/DATASET.csv");
             Scanner reader = new Scanner(csvFile);
             List<peopleData> p = new ArrayList<peopleData>(); 
 
+            // Pula a primeira linha do arquivo para ignorar título planilha
+            if(reader.hasNextLine()) {
+            reader.nextLine();
+            }
+
             while (reader.hasNextLine()) {
                 String line = reader.nextLine();
-                String[] fileDatas = line.split(",");
-                System.out.println(fileDatas.length);
-                if (fileDatas.length == 4) {
+                String[] fileDatas = line.split(";");
+                
                 String firstName = fileDatas[0];
                 String surname = fileDatas[1];
-                Float weight = Float.parseFloat(fileDatas[2]);
-                Float height = Float.parseFloat(fileDatas[3]);
-                
-                peopleData person = new peopleData(firstName, surname, weight, height);
-                p.add(person);
-                } else {
-                    //System.out.println("Invalid line " + line);
-                }
-                
+                float weight = Float.parseFloat(fileDatas[2].replace(",", "."));
+                float height = Float.parseFloat(fileDatas[3].replace(",", "."));
+
+                System.out.println("Nome: " + fileDatas[0]);
+                System.out.println("Sobrenome: " + fileDatas[1]);
+                System.out.println("Peso: " + fileDatas[2]);
+                System.out.println("Altura: " + fileDatas[3]);
+                System.out.println();
+
+               peopleData person = new peopleData(firstName, surname, weight, height);
+               p.add(person);
             }
             reader.close();
 
-            for (peopleData person : p) {
+            for (int i = 0; i < p.size(); i++) {
                 System.out.println("Nome: " + ((peopleData) p).getFirstName());
                 System.out.println("Sobrenome: " + ((peopleData) p).getSurname());
                 System.out.println("Peso: " + ((peopleData) p).getWeight());
                 System.out.println("Altura: " + ((peopleData) p).getHeight());
                 System.out.println();
-             }
+            }
 
          } catch (FileNotFoundException e) {
             System.out.println("Erro ao abrir o arquivo.");
